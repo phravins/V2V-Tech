6871eb43b4f8d6b882eb2617f2d23d13078771f7
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-01-31 19:52:01 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,22 +36,8 @@ const HeroSection = () => {
     >
       <div className="absolute top-5 right-5 z-50">
         <Link to="/admin-login">
-          <button className="cssbuttons-io-button">
+          <button className="admin-glow-btn">
             Admin Login
-            <div className="icon">
-              <svg
-                height="24"
-                width="24"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M0 0h24v24H0z" fill="none"></path>
-                <path
-                  d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </div>
           </button>
         </Link>
       </div>
